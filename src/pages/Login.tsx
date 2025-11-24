bdcbbaa8c65dfcd45f2bbb39ac945bedd8653383
Bambo <phamtanbach123@gmail.com>
2025-11-24 17:10:15 +0700

    fixed login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://localhost:5000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
